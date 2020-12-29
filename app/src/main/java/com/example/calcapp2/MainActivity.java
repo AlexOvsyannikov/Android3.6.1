884942977b507c8cf7a178b6d2eda36d954385c4
AlexOff <alexov2004@gmail.com>
2020-12-30 00:03:28 +0300

    Added variables for MainActivity

--- app/src/main/java/com/example/calcapp2/MainActivity.java
+++ app/src/main/java/com/example/calcapp2/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.calcapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //add variables
+    EditText arg1;
+    EditText arg2;
+    Button add;
+    Button subtr;
+    Button mul;
+    Button divide;
+    TextView answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
