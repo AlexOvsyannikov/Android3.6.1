38fdf289a64b39c24742c4720b691ade26ebce4d
AlexOff <alexov2004@gmail.com>
2020-12-30 14:31:31 +0300

    Button realisation started

--- app/src/main/java/com/example/calcapp2/MainActivity.java
+++ app/src/main/java/com/example/calcapp2/MainActivity.java
@@ -2,25 +2,32 @@ package com.example.calcapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
     //add variables
     EditText arg1;
     EditText arg2;
-    Button add;
-    Button subtr;
-    Button mul;
-    Button divide;
     TextView answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
+    public void get_data(){
+
+    }
+
+    public void add(View view) {
+
+    }
 }
