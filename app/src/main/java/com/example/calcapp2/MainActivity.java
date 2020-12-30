0e434d98d900722b221114378a7114fab1c0f06b
AlexOff <alexov2004@gmail.com>
2020-12-30 14:48:31 +0300

    Add button made

--- app/src/main/java/com/example/calcapp2/MainActivity.java
+++ app/src/main/java/com/example/calcapp2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calcapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,18 +17,40 @@ public class MainActivity extends AppCompatActivity {
     EditText arg2;
     TextView answer;
 
+    int Arg1;
+    int Arg2;
+    int Answer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        arg1 = (EditText)findViewById(R.id.arg1);
+        arg2 = (EditText)findViewById(R.id.arg2);
+        answer = (TextView) findViewById(R.id.answer);
     }
 
+    @SuppressLint("SetTextI18n")
     public void get_data(){
+        try {
+            this.Arg1 = Integer.parseInt(arg1.getText().toString());
+            this.Arg2 = Integer.parseInt(arg2.getText().toString());
+        }
+        catch (NumberFormatException nfe){
+            answer.setText("Input Error");
+        }
+    }
+
+    public void put_data(){
+        if (!answer.getText().toString().equals("Input Error")){
+            answer.setText(String.valueOf(this.Answer));
+        }
 
     }
 
     public void add(View view) {
-
+        get_data();
+        this.Answer=this.Arg1+this.Arg2;
+        put_data();
     }
 }
